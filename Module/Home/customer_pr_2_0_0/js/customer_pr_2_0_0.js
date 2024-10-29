a897271cd9302f5e52cd4f5d833f5b75618e232b
quangphe <minhquang1356@gmail.com>
2024-10-29 14:58:54 +0700

    upload website nha khoa paris

--- Module/Home/customer_pr_2_0_0/js/customer_pr_2_0_0.js
+++ Module/Home/customer_pr_2_0_0/js/customer_pr_2_0_0.js
@@ -1,35 +1,35 @@
 
 'use strict';
 
-const carouselItems = document.querySelectorAll('.customer_pr_2_0_0__itemCarousel');
-console.log(carouselItems)
-let currentItem = document.querySelector('.carouselCustomer--main');
-const leftBtn = document.querySelector('#leftBtnCustomer');
-const rightBtn = document.querySelector('#rightBtnCustomer');
+const carouselItemsCustomer = document.querySelectorAll('.customer_pr_2_0_0__itemCarousel');
+console.log(carouselItemsCustomer)
+let currentItemCustomer = document.querySelector('.carouselCustomer--main');
+const leftBtnCustomer = document.querySelector('#leftBtnCustomer');
+const rightBtnCustomer = document.querySelector('#rightBtnCustomer');
 
 
-rightBtn.addEventListener('click', function () {
-    currentItem = document.querySelector('.carouselCustomer--right');
-    const leftItem = document.querySelector('.carouselCustomer--main');
-    carouselItems.forEach((item, i) => {
+rightBtnCustomer.addEventListener('click', function () {
+    currentItemCustomer = document.querySelector('.carouselCustomer--right');
+    const leftItemCustomer = document.querySelector('.carouselCustomer--main');
+    carouselItemsCustomer.forEach((item, i) => {
         item.classList = 'customer_pr_2_0_0__itemCarousel';
     });
-    currentItem.classList.add('carouselCustomer--main');
-    leftItem.classList.add('carouselCustomer--left');
-    const currentId = Array.from(carouselItems).indexOf(currentItem);
-    const rightItem = currentId === carouselItems.length - 1 ? carouselItems[0] : carouselItems[currentId + 1];
-    rightItem.classList.add('carouselCustomer--right');
+    currentItemCustomer.classList.add('carouselCustomer--main');
+    leftItemCustomer.classList.add('carouselCustomer--left');
+    const currentIdCustomer = Array.from(carouselItemsCustomer).indexOf(currentItemCustomer);
+    const rightItemCustomer = currentIdCustomer === carouselItemsCustomer.length - 1 ? carouselItemsCustomer[0] : carouselItemsCustomer[currentIdCustomer + 1];
+    rightItemCustomer.classList.add('carouselCustomer--right');
 });
 
-leftBtn.addEventListener('click', function () {
-    currentItem = document.querySelector('.carouselCustomer--left');
-    const rightItem = document.querySelector('.carouselCustomer--main');
-    carouselItems.forEach((item, i) => {
+leftBtnCustomer.addEventListener('click', function () {
+    currentItemCustomer = document.querySelector('.carouselCustomer--left');
+    const rightItemCustomer = document.querySelector('.carouselCustomer--main');
+    carouselItemsCustomer.forEach((item, i) => {
         item.classList = 'customer_pr_2_0_0__itemCarousel';
     });
-    currentItem.classList.add('carouselCustomer--main');
-    rightItem.classList.add('carouselCustomer--right');
-    const currentId = Array.from(carouselItems).indexOf(currentItem);
-    const leftItem = currentId === 0 ? carouselItems[carouselItems.length - 1] : carouselItems[currentId - 1];
-    leftItem.classList.add('carouselCustomer--left');
+    currentItemCustomer.classList.add('carouselCustomer--main');
+    rightItemCustomer.classList.add('carouselCustomer--right');
+    const currentIdCustomer = Array.from(carouselItemsCustomer).indexOf(currentItemCustomer);
+    const leftItemCustomer = currentIdCustomer === 0 ? carouselItemsCustomer[carouselItemsCustomer.length - 1] : carouselItemsCustomer[currentIdCustomer - 1];
+    leftItemCustomer.classList.add('carouselCustomer--left');
 });
